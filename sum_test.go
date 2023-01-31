a4b7cd7dec5c02e782977736f4899446e7e67596
Marcelo Nepomuceno <marcelonepomucenoti@gmail.com>
2023-01-31 11:28:40 -0300

    feat: :sparkles: Remove funcs

--- sum_test.go
+++ sum_test.go
@@ -9,11 +9,3 @@ func TestSum(t *testing.T) {
 		t.Error("The result must be 5")
 	}
 }
-
-func TestSub(t *testing.T) {
-	result := sub(3, 2)
-
-	if result != 1 {
-		t.Error("The result must be 1")
-	}
-}
